68f5ac8f6ec9e443262c936ab3d89b9f621783e2
Christian Aguilar <cbaguilar@ucla.edu>
2020-10-01 19:19:15 -0700

    LIDAR SENSOR TEST

---
+++ hardwareDemos/vl53l0x/vl53l0x.ino
@@ -0,0 +1,92 @@
+#include "Adafruit_VL53L0X.h"
+
+
+
+
+#define IN1 0
+#define IN2 4
+#define IN3 18
+#define IN4 5
+
+#define LEDC_CHANNEL_1     1
+#define ENB     19
+
+
+ 
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+  // calculate duty, 8191 from 2 ^ 13 - 1
+  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+
+  // write duty to LEDC
+  ledcWrite(channel, duty);
+}
+
+// use first channel of 16 channels (started from zero)
+#define LEDC_CHANNEL_0     0
+
+// use 13 bit precission for LEDC timer
+#define LEDC_TIMER_13_BIT  13
+
+// use 5000 Hz as a LEDC base frequency
+#define LEDC_BASE_FREQ     5000
+
+// fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
+#define LED_PIN            2
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+
+
+  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
+
+
+ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+  ledcAttachPin(ENB, LEDC_CHANNEL_1);
+
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  
+}
+
+int incomingByte = 0;
+
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  //Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+ 
+  ledcAnalogWrite(LEDC_CHANNEL_0, measure.RangeMilliMeter);
+  ledcAnalogWrite(LEDC_CHANNEL_1, measure.RangeMilliMeter);
+
+    
+  delay(100);
+}
