28fad005436cc46b8e256969405c9de29d6fd573
Christian Aguilar <cbaguilar@ucla.edu>
2020-11-11 21:07:44 -0800

    Moved to separate turnFunctions2 folder

---
+++ octCode/turnFunctions2/turnFunctions2.h
@@ -0,0 +1,7 @@
+#include <cstdint> 
+
+void moveForward(int steps, int mspeed);
+void turnLeft(int steps, int mspeed);
+void turnRight(int steps, int mspeed);
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255);
+void setupFunction();
