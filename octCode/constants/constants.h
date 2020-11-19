32906b391272e6922f81b73891d5c5a10030196e
Christian Aguilar <cbaguilar@ucla.edu>
2020-11-18 17:08:34 -0800

    Added constants

---
+++ octCode/constants/constants.h
@@ -0,0 +1,27 @@
+/**
+ *constants.h
+*
+* Contains operational constants and useful values for 2020 lunabotics
+*
+*
+* HI THIS WAS CHANGED ON THE ORIGINAL
+*
+ */
+
+#define LEDC_CHANNEL_0     0
+#define LEDC_CHANNEL_1     1
+#define IN1 2
+#define IN2 3
+#define IN3 4
+#define IN4 5
+
+#define ENA 10
+#define ENB 11
+
+#define MAX_SPEED_RAD 5.23598775 
+#define MAX_SPEED_RPM 50
+
+#define MAX_SPEED_MPS .157077
+
+
+#define WHEEL_CIRCUMFERENCE_M .377
