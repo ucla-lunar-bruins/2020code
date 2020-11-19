57b0a27b9c4c3cbd7930291d4f3a0d8a1bd99244
Daniel-Ferguson1 <33245718+Daniel-Ferguson1@users.noreply.github.com>
2020-11-18 19:07:45 -0500

    Add files via upload

---
+++ octCode/dualSensorRead.h
@@ -0,0 +1,5 @@
+#include "Adafruit_VL53L0X.h"
+
+void initSensors(hex address1, hex address1, int shutdownPin1, int shutdownPin2, Adafruit_VL53L0X lox1, Adafruit_VL53L0X lox2);
+
+int readSensor(Adafruit_VL53L0X lox); 
